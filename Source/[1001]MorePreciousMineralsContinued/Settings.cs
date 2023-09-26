a6fcb24f3864ad1699e3cf4b4c011b940819a622
papercrane1001 <papercrane1001@gmail.com>
2023-09-26 08:51:22 -0700

    Just did some testing, but the remembering thing seems to be fixed?  Will do more testing.

--- Source/[1001]MorePreciousMineralsContinued/Settings.cs
+++ Source/[1001]MorePreciousMineralsContinued/Settings.cs
@@ -19,28 +19,30 @@ namespace _1001_MorePreciousMineralsContinued
         public static int valueRangeMin { get { return 5000 * (1 + percentIncrease / 100) - 1500; } }
         public static int valueRangeMax { get { return 5000 * (1 + percentIncrease / 100) + 1500; } }
 
-        //public static 
 
         public override void ExposeData()
         {
             //Log.Message("Point 0");
             base.ExposeData();
-            //Log.Message("Point 1");
+            //Log.Message("Point 1: " + percentIncrease.ToString());
             Scribe_Values.Look(ref percentIncrease, "MorePreciousMineralsContinued.percentIncrease", 50);
-            //Log.Message("Point 2");
-            if(Scribe.mode == LoadSaveMode.PostLoadInit)
+            //Log.Message("Point 2: " + percentIncrease.ToString());
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
             {
+                //Log.Message("Point 3");
                 if(percentIncrease < 0) { percentIncrease = 0; }
                 else if(percentIncrease >= 500) { percentIncrease = 500; }
+                //Log.Message("Point 4: " + percentIncrease.ToString());
                 SettingsController.percentIncreaseBuffer = percentIncrease.ToString();
-
-                if(DefDatabase<GenStepDef>.AllDefs.Count() > 0)
+                //Log.Message("Point 5: " + percentIncrease.ToString());
+                if (DefDatabase<GenStepDef>.AllDefs.Count() > 0)
                 {
                     GenStepDef def = DefDatabase<GenStepDef>.GetNamed("PreciousLump");
                     if (def.genStep != null && def.genStep as GenStep_PreciousLump != null)
                     {
                         GenStep_PreciousLump gsLump = (GenStep_PreciousLump)def.genStep;
                         gsLump.totalValueRange = new FloatRange(valueRangeMin, valueRangeMax);
+                        //Log.Message("Point 6: " + percentIncrease.ToString());
                     }
                 }
                 
@@ -66,16 +68,17 @@ namespace _1001_MorePreciousMineralsContinued
         public override void DoSettingsWindowContents(UnityEngine.Rect inRect)
         {
             //Log.Message("Point3");
-
+            //Log.Message("Point 7: " + Settings.percentIncrease.ToString());
             Widgets.Label(new UnityEngine.Rect(inRect.xMin, inRect.yMin, 100, 32), "MorePreciousMineralsContinued.percentIncrease".Translate());
             percentIncreaseBuffer = Widgets.TextField(new Rect(inRect.xMin + 110, inRect.yMin, 100, 32), percentIncreaseBuffer);
-            if(int.TryParse(percentIncreaseBuffer.Trim(), out int i))
+            //Log.Message("Point 8: " + percentIncreaseBuffer.ToString());
+            if (int.TryParse(percentIncreaseBuffer.Trim(), out int i))
             {
                 if(i > 0 && i <= 500) { Settings.percentIncrease = i; }
                 else if(percentIncreaseBuffer.Trim() == "") { Settings.percentIncrease = 0; }
                 percentIncreaseBuffer = Settings.percentIncrease.ToString();
             }
-            Log.Message(Settings.percentIncrease.ToString());
+            //Log.Message(Settings.percentIncrease.ToString());
             GenStepDef def = DefDatabase<GenStepDef>.GetNamed("PreciousLump");
             if (def.genStep as GenStep_PreciousLump != null)
             {
